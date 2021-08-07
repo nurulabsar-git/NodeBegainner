f0afe0cac2a23e6ee4693ca83f5ef66c5f829a46
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-08-06 23:35:03 -0700

    path buildin module

---
+++ BuildInModulePath.js
@@ -0,0 +1,11 @@
+const path = require('path');
+// console.log(path.sep);
+// console.log(path.join);
+// console.log(path.delimiter);
+// console.log(path. win32);
+
+
+// join() method join file location
+
+const filePath = path.join('/content', 'subContent', 'test.txt')
+console.log(filePath);
