c5e1c3a012e959092b9a8673623d1a71bc04f586
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-08-07 00:35:02 -0700

    fs red and write file Sync in buildin module

--- BuildInModuleFs.js
+++ BuildInModuleFs.js
@@ -4,6 +4,8 @@ const first = readFileSync('./content/first.txt', 'utf-8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
 console.log(first, second);
+setInterval(() => {
+    writeFileSync('./content/iDoNotWrite.txt', `Here is the result: ${first}, ${second}... \n\t`, {flag: 'a'} )
+}, 1000)
 
-writeFileSync('./content/iDoNotWrite.txt', `Here is the result: ${first}, ${second}`)
 writeFileSync('./content/iDoNotWrite.js', `//Here is the result: ${first}, ${second}`)
