d1c56efd7aebbb4d4384148a56adaaceb8b3dd50
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-08-06 17:40:39 -0700

    nodeBegainner

---
+++ App.js
@@ -0,0 +1,14 @@
+console.log(__dirname);
+// console.log(__fileName);
+setInterval(()=> {
+    console.log('Hello Bangladesh');
+}, 1000)
+
+console.log('Hello WORLD');
+
+
+/* So this is called Single threated.....setInterval() allows asynchronous, 
+finally filled up event loop!(javascript execute line by line)*/ 
+
+
+
