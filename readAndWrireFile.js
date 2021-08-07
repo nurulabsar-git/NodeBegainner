77e0503c73c0c48e98d53be070fdcf78335ebedf
nurulabsar-git <mohammad.nurulabsar20203025@yahoo.com>
2021-08-07 11:56:16 -0700

    readAndWrireFile.js

---
+++ readAndWrireFile.js
@@ -0,0 +1,32 @@
+const {readFile, writeFile} = require('fs')
+
+readFile('./anotherContent/first.txt', 'utf8', (err, result) => {
+    if(err){
+        console.log(err);
+        return
+    }
+  console.log(result);
+  const first = result;
+  readFile('./anotherContent/second.txt', 'utf8', (err, result) => {
+      if(err){
+          console.log(err);
+          return
+      }
+   console.log(result);
+  const second = result
+
+  setInterval(() =>{
+    writeFile('./anotherContent/iDoNotWrite.txt', 
+    `Here is the result: ${first} ${second} \n\t`, {flag: 'a'}, (err, result) => {
+        if(err){
+            return;
+        }
+  
+      //   console.log(result);
+    })
+  }, 1000)
+
+
+  })
+
+})
